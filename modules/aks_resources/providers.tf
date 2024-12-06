c8e5fd3c6d270be784b10bef31e17b62924e4254
unknown <tomislav.skrgulja@intelligentsolutions.no>
2024-12-06 12:32:40 +0100

    removed azurerm auth details

--- modules/aks_resources/providers.tf
+++ modules/aks_resources/providers.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features {}
-
-  tenant_id       = "542a87e8-9739-4279-83af-6eae28903ccd"
-  client_id       = "debb0b5e-1096-49da-bd2d-e98187685849"
-  client_secret   = "CZ98Q~RD2z2ZYXc_3gWt11VLy4SuVK8Xj4__lcpd"
-  subscription_id = "bfa5fa76-965d-497d-a2c3-300cd6dfb70b"
 }
 
 provider "kubernetes" {
