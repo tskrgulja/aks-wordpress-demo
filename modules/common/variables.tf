01ee5eb5cac8f14dc5baa213f5269217a1cb06a6
unknown <tomislav.skrgulja@intelligentsolutions.no>
2024-12-08 23:11:15 +0100

    Added variable descriptions.

--- modules/common/variables.tf
+++ modules/common/variables.tf
@@ -1,7 +1,9 @@
 variable "project_name" {
   type = string
+  description = "Name of the project to be deployed. Used in resource naming."
 }
 
 variable "environment" {
   type = string
+  description = "Name of the environment (for example 'dev', 'test' etc.). Used in resource naming."
 }
--- modules/infrastructure/variables.tf
+++ modules/infrastructure/variables.tf
@@ -1,32 +1,41 @@
 variable "subscription_id" {
   type = string
+  description = "ID of the Azure subscription."
 }
 
 variable "project_name" {
   type = string
+  description = "Name of the project to be deployed. Used in resource naming."
 }
 
 variable "environment" {
   type = string
+  description = "Name of the environment (for example 'dev', 'test' etc.). Used in resource naming."
 }
 
 variable "location" {
   type = string
-}
+  description = "Azure location for the resources to be deployed. Used in resource naming."
 
-variable "db_username" {
-  type = string
 }
 
 variable "db_password" {
   type      = string
   sensitive = true
+  description = "Password to be used for Wordpress database server administrator account."
+}
+
+variable "db_username" {
+  type = string
+  description = "Username to be used for Wordpress database server administrator account."
 }
 
 variable "wordpress_domain_name" {
   type = string
+  description = "Public domain name for the Wordpress application."
 }
 
 variable "dns_zone_domain_name" {
   type = string
+  description = "Domain name of the Azure DNS Zone which will hold records for the Wordpress domain."
 }
